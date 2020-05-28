caaba15f71c378c2ff48d7a7fc858a841390a271
lucas araujo <lucascorrea335@hotmail.com>
2020-05-28 20:22:02 -0300

    Second

---
+++ AdegaZeRatao/src/components/routes.js
@@ -0,0 +1,23 @@
+// //Arquivo responsável pelas rotas.
+
+// import React from 'react';
+// import {BrowserRouter, Switch, Route} from 'react-router-dom';
+// import Criacao from '/Projeto_Adega/adegazeratao/src/components/Criacao';
+
+// import App from '/Projeto_Adega/adegazeratao/src/App';
+
+// //Criar o componentes com as rotas
+// const Routes = () => {
+//         return(
+//             <BrowserRouter>
+//                 <Switch>
+//                     <Route exact path='/' component={App}/>
+//                     <Route path='/Criacao' component={Criacao}/>
+//                 </Switch>
+//             </BrowserRouter>
+
+//         );
+    
+// }
+
+// export default Routes;
